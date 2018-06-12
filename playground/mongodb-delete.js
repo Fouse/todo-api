9d8969394f004c93c9b21c58aa3760b3146ab6a5
Fousseini Konate <Fous@Fousseinis-MacBook-Air.local>
2018-06-12 00:42:05 -0400

    added deleted functionality

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,22 @@
+const { MongoClient, ObjectId } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+    //deleteMany
+    // db.collection('Todos').deleteMany({ text: 'Eat lunch' }).then((result) => {
+    //     console.log(result);
+    // });
+    //deleteOne
+    // db.collection('Todos').deleteOne({ text: 'Eat dinner' }).then((result) => {
+    //     console.log(result);
+    // });
+    //findOneAndDelete
+    db.collection('Todos').findOneAndDelete({ text: 'Eat dinner' }).then((result) => {
+        console.log(result);
+    });
+    //client.close();
+});
